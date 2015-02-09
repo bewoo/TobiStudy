fcf196ec7fc09fbccf32ba8802f522de7c7ebfe0
abyungeun@gmail.com <abyungeun@gmail.com>
2015-02-10 01:07:01 +0900

    2015.02.10 commit

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -4,6 +4,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import java.sql.ResultSet;
@@ -25,27 +26,42 @@ public class UserDaoJdbc implements UserDao{
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
+            user.setLevel(Level.valueOf(rs.getInt("level")));
+            user.setLogin(rs.getInt("login"));
+            user.setRecommend(rs.getInt("recommend"));
             return user;
         }
     };
 
+    @Override
     public void add(User user) throws DuplicateKeyException {
-        this.jdbcTemplate.update("insert into users" + "(id, name, password)" + "values" + "(?,?,?)", user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users (id, name, password, level , login, recommend) values (?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
     }
 
+    @Override
     public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, this.userMapper);
     }
 
+    @Override
     public void deleteAll() {
         this.jdbcTemplate.update("delete from users");
     }
 
+    @Override
     public int getCount() {
         return this.jdbcTemplate.queryForInt("select count(*) from users");
     }
 
+    @Override
     public List<User> getAll() {
        return  this.jdbcTemplate.query("select * from users order by id", this.userMapper);
+    }
+
+    @Override
+    public void update(User user) {
+        jdbcTemplate.update("update users set name = ?, password = ?, level = ?, login = ?, recommend = ? where id = ?",
+                user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getId());
     }
 }
--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -4,6 +4,9 @@ public class User {
     String id;
     String name;
     String password;
+    Level level;
+    int login;
+    int recommend;
 
     /**
      * 자바빈의 규약을 따르는 디폴트 생성자
@@ -11,10 +14,37 @@ public class User {
     public User() {
     }
 
-    public User(String id, String name, String password) {
+    public User(String id, String name, String password, Level level, int login, int recommend) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.level = level;
+        this.login = login;
+        this.recommend = recommend;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
     }
 
     public String getId() {
@@ -34,5 +64,9 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void update(User user1) {
+
     }
 }
