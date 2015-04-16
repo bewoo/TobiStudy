4735328599aecc0db6e20388013261813821c19f
Woo <abyungeun@gmail.com>
2015-04-16 18:02:22 +0900

    2015.04.16 자기참조빈(590)

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -15,10 +15,20 @@ import java.util.Map;
 /**
  * Created by Woo on 2015-04-15.
  */
-public class XmlSqlService implements SqlService {
+public class XmlSqlService implements SqlService, SqlRegistry, SqlReader {
 
     private Map<String, String> sqlMap = new HashMap<String, String>();
     private String sqlmapFile;
+    private SqlReader sqlReader;
+    private SqlRegistry sqlRegistry;
+
+    public void setSqlReader(SqlReader sqlReader) {
+        this.sqlReader = sqlReader;
+    }
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
 
     public void setSqlmapFile(String sqlmapFile) {
         this.sqlmapFile = sqlmapFile;
@@ -26,13 +36,27 @@ public class XmlSqlService implements SqlService {
 
     @Override
     public String getSql(String key) throws SqlRetrievalFailureException {
+        try {
+            return sqlRegistry.findSql(key);
+        } catch (SqlNotFoundException e) {
+            throw new SqlRetrievalFailureException(e);
+        }
+    }
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
         String sql = sqlMap.get(key);
         if(sql == null) throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
         else return sql;
     }
 
-    @PostConstruct
-    public void loadSql() {
+    @Override
+    public void registerSql(String key, String sql) {
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
@@ -41,11 +65,15 @@ public class XmlSqlService implements SqlService {
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for(SqlType sql : sqlmap.getSql()) {
-                sqlMap.put(sql.getKey(), sql.getValue());
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
             }
         } catch (JAXBException e) {
             throw new RuntimeException();
         }
     }
 
+    @PostConstruct
+    public void loadSql() {
+        this.sqlReader.read(this.sqlRegistry);
+    }
 }
